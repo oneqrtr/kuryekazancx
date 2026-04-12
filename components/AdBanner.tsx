c3e3c4f6b614ef6409f55be4318fd50e3e84635f
heloris <oneqrtr@gmail.com>
2026-04-12 13:35:27 +0300

    adc

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -1,8 +1,3 @@
 export function AdBanner({ className = '' }: { className?: string }) {
-  return (
-    <div className={`w-full bg-gray-100 border border-dashed border-gray-300 rounded-lg flex items-center justify-center p-4 min-h-[120px] text-gray-400 text-sm font-medium ${className}`}>
-      {/* TODO: Google AdSense kodları burata eklenecek */}
-      [Görsel Reklam Alanı]
-    </div>
-  );
+  return <div className={`w-full ${className}`} />;
 }
